1c10e8c18053515e7971249b02d0c62d98d6344b
Ederson Lucas <edersonlucas@outlook.com.br>
2022-03-07 18:54:51 -0300

    exercicio1

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercicio1.js
@@ -0,0 +1,17 @@
+const a = 10
+const b = 3
+
+// Programa 1 Adição
+console.log(a + b)
+
+// Programa 2 Subtração
+console.log(a - b)
+
+// Programa 3 Multiplicação
+console.log(a * b)
+
+// Programa 4 Divisão
+console.log(a / b)
+
+// Programa 5 Módulo
+console.log(a % b)
