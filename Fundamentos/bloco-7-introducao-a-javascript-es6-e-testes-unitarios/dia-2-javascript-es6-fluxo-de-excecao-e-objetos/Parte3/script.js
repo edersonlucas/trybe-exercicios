9cbc94cb8a2f576472fa1b8977e2c2ea3c245e42
Ederson Lucas <edersonlucas@outlook.com.br>
2022-04-05 20:13:51 -0300

    Exercicios/7.2

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/Parte3/script.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/Parte3/script.js
@@ -43,13 +43,117 @@ const valueToObject = object => Object.values(object).forEach(Value => console.l
 
 valueToObject(lesson3)
 
-const allLessons = {
-  lesson1: {},
-  lesson2: {},
-  lesson3: {},
-}
-allLessons.lesson1 = Object.assign({}, lesson1)
-allLessons.lesson2 = Object.assign({}, lesson2)
-allLessons.lesson3 = Object.assign({}, lesson3)
+// Exercicio 5
 
-console.log(allLessons);
+
+// const allLessons = {
+//   lesson1: {},
+//   lesson2: {},
+//   lesson3: {},
+// }
+// allLessons.lesson1 = Object.assign({}, lesson1)
+// allLessons.lesson2 = Object.assign({}, lesson2)
+// allLessons.lesson3 = Object.assign({}, lesson3)
+
+// console.log(allLessons);
+
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+
+// Exercicio 6
+// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const totalEstudandes = objeto => {
+  let somatorio = 0
+  for (let i = 1; i <= Object.keys(objeto).length; i += 1) {
+    somatorio += objeto[`lesson${i}`].numeroEstudantes
+  }
+  return somatorio
+}
+
+console.log(totalEstudandes(allLessons));
+
+// Exercicio 7
+
+const getValueByNumber = (objeto, posicao) => Object.values(objeto)[posicao]
+
+
+console.log(getValueByNumber(lesson1, 0));
+
+// Exercicio 8
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+
+// const verifyPair = (objeto, chave, valor) => {
+//   let contador;
+//   const array = Object.entries(objeto)
+//   array.forEach(elemento => {
+//     contador = 0
+//     elemento.forEach(outro => {
+//       if (outro === chave || outro === valor) {
+//         contador += 1
+//       }
+//     })
+//   })
+//   if (contador === 2) {
+//     return true
+//   } else {
+//     return false
+//   }
+// };
+// console.log(verifyPair(lesson3, 'turno', 'noite'));
+// console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+
+const verifyPair = (objeto, chave, valor) => {
+  const chaveAndValue = Object.entries(objeto)
+  for (let i = 0; i < chaveAndValue.length; i += 1) {
+    if (chaveAndValue[i][0] === chave && chaveAndValue[i][1] === valor) {
+      return true
+    }
+  }
+  return false
+}
+
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+// Output: true,
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+// Output: false
+
+
+// Bônus
+
+// 1 - Crie uma função para contar quantos estudantes assistiram às aulas de Matemática.Use o objeto criado no exercício 5.
+
+
+const qtAssistiuAula = (objeto, materia) => {
+  let total = 0
+  Object.keys(objeto).forEach(key => {
+    console.log(Object.keys(objeto[key]));
+    if (Object.values(objeto[key]).includes(materia)) {
+      total += Object.values(objeto[key])[1]
+    }
+  })
+  return total
+}
+
+console.log(qtAssistiuAula(allLessons, 'Matemática'));
+
+// 2 - Crie uma função que deverá retornar um objeto que representa o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes. Use o objeto criado no exercício 5:
+
+
+const createReport = (objeto, professor) => {
+  const novo = {}
+  const aula = []
+  let total = 0
+  Object.keys(objeto).forEach(key => {
+    console.log(Object.keys(objeto[key]));
+    if (Object.values(objeto[key]).includes(professor)) {
+      aula.push(novo.professor = Object.values(objeto[key])[0])
+      total += Object.values(objeto[key])[1]
+      novo.professor = Object.values(objeto[key])[2]
+      novo.aulas = aula
+      novo.estudantes = total
+    }
+  })
+  return novo
+}
+
+console.log(createReport(allLessons, 'Maria Clara'))
